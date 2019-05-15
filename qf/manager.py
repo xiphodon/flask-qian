1a55a16ab11aed6a69e66ec1137aca85d9d0e42c
GuoChang <827789895@qq.com>
2019-05-15 17:53:44 +0800

    反向解析

--- qf/manager.py
+++ qf/manager.py
@@ -5,16 +5,36 @@
 # @Site    : https://github.com/xiphodon
 # @File    : manager.py
 # @Software: PyCharm
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_script import Manager
 
 app = Flask(__name__)
 manager = Manager(app=app)
 
 
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html')
+@app.route('/hello/<string:name>/', methods=['GET'])
+def hello(name):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/any/<any(i, you, he, "123"):name>/', methods=['GET', 'POST'])
+def hello_any(name):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/url/')
+def get_url():
+    """
+    反向解析
+    :return:
+    """
+    url = url_for('hello_any', name='you')  # /any/you/
+    return url
+
+
+@app.route('/getphone/')
+def get_phone():
+    return '139-0000-1111'
 
 
 if __name__ == '__main__':
