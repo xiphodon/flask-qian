cb12f727a19d5992e1a859ce5cd153f68c1c4694
GuoChang <827789895@qq.com>
2019-05-17 14:24:48 +0800

    template

--- qf/module1/views_1.py
+++ qf/module1/views_1.py
@@ -5,7 +5,7 @@
 # @Site    : https://github.com/xiphodon
 # @File    : views_1.py
 # @Software: PyCharm
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('bp_1', __name__, url_prefix='/m1')
 
@@ -13,3 +13,16 @@ bp = Blueprint('bp_1', __name__, url_prefix='/m1')
 @bp.route('/index/')
 def index():
     return f'hello {__name__}'
+
+
+@bp.route('/index_b/')
+def index_b():
+    return render_template('index_b.html')
+
+
+@bp.route('/index_b2/')
+def index_b2():
+    names = ['Lucy', 'Lili', 'Nick', 'Tom']
+    numbers = range(1, 4)
+    html_str = '<h1>html str</h1>'
+    return render_template('index_b2.html', names=names, numbers=numbers, html_str=html_str)
